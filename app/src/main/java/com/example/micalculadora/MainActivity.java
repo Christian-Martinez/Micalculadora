285ee0d59245c992a2d4d406cf9be290f59cbefd
Christian-Martinez <christ96mart@gmail.com>
2019-03-30 21:18:34 -0400

    funcionar botones numericos y punto

--- app/src/main/java/com/example/micalculadora/MainActivity.java
+++ app/src/main/java/com/example/micalculadora/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.micalculadora;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        configEditText();
+    }
+
+    private void configEditText() {
+
+    }
+
+    @OnClick({R.id.btnSeven, R.id.btnFour, R.id.btnOne, R.id.btnEight, R.id.btnFive, R.id.btnTwo,
+            R.id.btnNine, R.id.btnSix, R.id.btnThree, R.id.btnPoint, R.id.btnZero})
+    public void onClickNumbers(View view) {
+        final String valStr = ((Button) view).getText().toString();
+        switch (view.getId()) {
+            case R.id.btnZero:
+            case R.id.btnOne:
+            case R.id.btnTwo:
+            case R.id.btnThree:
+            case R.id.btnFour:
+            case R.id.btnFive:
+            case R.id.btnSix:
+            case R.id.btnSeven:
+            case R.id.btnEight:
+            case R.id.btnNine:
+                etInput.getText().append(valStr);
+                break;
+            case R.id.btnPoint:
+                final String operacion = etInput.getText().toString();
+
+                final String operador = Metodos.getOperator(operacion);
+
+                final int count = operacion.length() - operacion.replace(".", "").length();
+
+                if (!operacion.contains(Constantes.POINT) ||
+                        (count < 2 && (!operador.equals(Constantes.OPERATOR_NULL)))) {
+                    etInput.getText().append(valStr);
+                }
+                break;
+        }
+    }
+
+    @OnClick({R.id.btnClear, R.id.btnDiv, R.id.btnMultiplication, R.id.btnSub, R.id.btnSum, R.id.btnResult})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnClear:
+                break;
+            case R.id.btnDiv:
+                break;
+            case R.id.btnMultiplication:
+                break;
+            case R.id.btnSub:
+                break;
+            case R.id.btnSum:
+                break;
+            case R.id.btnResult:
+                break;
+        }
     }
 }
